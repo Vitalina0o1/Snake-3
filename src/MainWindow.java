00b86b282b153554cfad8285cb8797916a6bab51
Vitalina0o1 <k.v.s12@mail.ru>
2025-05-14 22:02:13 +0400

    я

---
+++ src/MainWindow.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+
+
+public class MainWindow extends JFrame {
+
+    public MainWindow(){
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320,345);
+        setLocation(400,400);
+        add(new GameField());
+        setVisible(true);
+        setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+    }
+}
